e8a3a0464a992a2e1066902617146c0cdb00c4b0
Alan O Shea <alanosheaclash@gmail.com>
2020-01-15 10:38:30 +0000

    Second variable added and tested

--- spec/drinkSpec.js
+++ spec/drinkSpec.js
@@ -2,7 +2,11 @@ describe("whatCanIDrink", function() {
     describe("Additional tests", function() {
         it("should return Sorry. I can’t tell what drink because that age is incorrect! when called as whatCanIDrink()", function() {
             var result = whatCanIDrink(-1)
-            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!")
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it("should return Drink Toddy when called as whatCanIDrink()", function(){
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
         });
     });
 });
